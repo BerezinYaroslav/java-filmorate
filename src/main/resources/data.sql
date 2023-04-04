e8cc9b866a526ddca7aecdb786139e9e583ed1f2
Yaroslav Berezin <yb2003work@gmail.com>
2023-04-05 01:09:56 +0300

    feat ТЗ-11: add sql files, new dependencies and properties

---
+++ src/main/resources/data.sql
@@ -0,0 +1,35 @@
+-- заполняем таблицу rating
+INSERT INTO "rating" (NAME)
+VALUES ('G');
+
+INSERT INTO "rating" (NAME)
+VALUES ('PG');
+
+INSERT INTO "rating" (NAME)
+VALUES ('PG-13');
+
+INSERT INTO "rating" (NAME)
+VALUES ('R');
+
+INSERT INTO "rating" (NAME)
+VALUES ('NC-17');
+
+
+-- заполняем таблицу genre
+INSERT INTO "genre" (NAME)
+VALUES ('Комедия');
+
+INSERT INTO "genre" (NAME)
+VALUES ('Драма');
+
+INSERT INTO "genre" (NAME)
+VALUES ('Мультфильм');
+
+INSERT INTO "genre" (NAME)
+VALUES ('Триллер');
+
+INSERT INTO "genre" (NAME)
+VALUES ('Документальный');
+
+INSERT INTO "genre" (NAME)
+VALUES ('Боевик');
