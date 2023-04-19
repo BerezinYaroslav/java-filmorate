5c7b89b039f23850244a74ec9e17d3b1f7a5df57
Yaroslav Berezin <yb2003work@gmail.com>
2023-04-20 01:30:49 +0300

    ТЗ-11

--- src/test/java/ru/yandex/practicum/filmorate/db/LikeStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/db/LikeStorageTest.java
@@ -15,10 +15,6 @@ import ru.yandex.practicum.filmorate.storage.UserStorage;
 
 import java.time.LocalDate;
 import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 @AutoConfigureTestDatabase
